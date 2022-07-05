6d2787a01876d50c7d42da13767ab00ce333661d
William Jones <williamseejones793@gmail.com>
2022-07-05 12:44:22 -0400

    changed the erb files to slim

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,8 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root "application#hello"
+  resources :users
+  root "users#index"
 
   get '/styleguide', to: 'styleguide#index'
 end
