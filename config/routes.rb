dffc2a144e2255132ca0a8875877e09e02baf6ff
William Jones <williamseejones793@gmail.com>
2022-07-05 11:02:19 -0400

    added hello world to the root

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "application#hello"
 
   get '/styleguide', to: 'styleguide#index'
 end
